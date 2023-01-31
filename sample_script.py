5e8ee444212b10d17ca8ab5f6e4b08f595281b73
Michael <drmichaelige@gmail.com>
2023-01-31 14:31:17 -0500

    This is a test commit

--- sample_script.py
+++ sample_script.py
@@ -1,9 +1,11 @@
 from time import sleep
+
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path="C:\\GitHub\\python-selenium-automation\\chromedriver.exe")
 driver.maximize_window()
 
 # open the url
